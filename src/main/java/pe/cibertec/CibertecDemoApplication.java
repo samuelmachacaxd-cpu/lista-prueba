6e367278268cac04e71630112a9883500383eaff
= <samuelmachacaxd@gmail.com>
2026-07-11 16:48:09 -0500

    creacion de proyecto

---
+++ src/main/java/pe/cibertec/CibertecDemoApplication.java
@@ -0,0 +1,11 @@
+package pe.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibertecDemoApplication {
+    public static void main(String[] args){
+        SpringBootApplication.run(CibertecDemoApplication.class, args);
+    }
+}
